3dec4b98fb746625435be0f549bc441d0753fd7a
micahzev <friedland.micah@gmail.com>
2016-03-15 12:13:36 +0200

    cleaned up MyShops imports

--- src/components/MyShops.js
+++ src/components/MyShops.js
@@ -1,10 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
-import {getCurrentUserId, getCurrentShopIds, getMyShops, getUser} from '../helpers/componentHelpers';
-import { bindActionCreators } from 'redux';
 import Navbar from '../Navbar.js';
-import { addShop } from '~/src/actions/shops';
 import MyShopsFetchShops from '~/src/components/MyShopsFetchShops';
 import MyShopsAddShop from '~/src/components/MyShopsAddShop';
 
