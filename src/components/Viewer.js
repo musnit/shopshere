2aace0376044a934c3c7b2c2eecb34fbb95035e4
micahzev <friedland.micah@gmail.com>
2016-03-18 14:36:47 +0200

    viewer resizes on window resize, click and drag image into viewer

--- src/components/Viewer.js
+++ src/components/Viewer.js
@@ -183,6 +183,49 @@ class Viewer extends Component {
 
 		document.addEventListener("mouseup", onDocumentMouseUp, false);
 
+
+		document.addEventListener( 'dragover', function ( event ) {
+
+					event.preventDefault();
+					event.dataTransfer.dropEffect = 'copy';
+
+				}, false );
+
+
+		document.addEventListener( 'dragenter', function ( event ) {
+
+					document.body.style.opacity = 0.5;
+
+				}, false );
+
+		document.addEventListener( 'dragleave', function ( event ) {
+					document.body.style.opacity = 1;
+				}, false );
+
+		document.addEventListener( 'drop', function ( event ) {
+					event.preventDefault();
+					var reader = new FileReader();
+
+					reader.addEventListener( 'load', function ( event ) {
+
+						sphereMaterial.map.image.src = event.target.result;
+						sphereMaterial.map.needsUpdate = true;
+
+					}, false );
+
+					reader.readAsDataURL( event.dataTransfer.files[ 0 ] );
+					document.body.style.opacity = 1;
+				}, false );
+				//
+
+		window.addEventListener( 'resize', onWindowResize, false );
+
+		function onWindowResize() {
+				camera.aspect = window.innerWidth / window.innerHeight;
+				camera.updateProjectionMatrix();
+				renderer.setSize( window.innerWidth, window.innerHeight );
+			}
+
 		render();
 
 
