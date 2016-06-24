0362386ea4830ed0a2b9fa87522c10f8165dd200
QuintinS <quintin.schnehage@gmail.com>
2016-06-24 13:04:43 +0200

    Bootstrap Styles

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -28,9 +28,12 @@ module.exports = {
       loader: 'style!css' // Run both loaders
     },
     { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,   loader: 'url?limit=10000&mimetype=application/font-woff' },
-    { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,   loader: 'url?limit=10000&mimetype=application/font-woff' },
+    { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,  loader: 'url?limit=10000&mimetype=application/font-woff' },
     { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,    loader: 'url?limit=10000&mimetype=application/octet-stream' },
     { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,    loader: 'file-loader' },
-    { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,    loader: 'url?limit=10000&mimetype=image/svg+xml' }
+    { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,    loader: 'url?limit=10000&mimetype=image/svg+xml' },
+    { test: /\.png(\?v=\d+\.\d+\.\d+)?$/,       loader: 'url?limit=10000!img?progressive=true' }
+    // { test: /\.(jpe?g|png|gif|svg)$/i,       loader: 'url?limit=10000!img?progressive=true' }
+    // { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
   ]}
 };
