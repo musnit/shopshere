faec412cabf33832e8697b56fd363a2b65eedc1c
QuintinS <quintin.schnehage@gmail.com>
2016-07-11 11:13:18 +0200

    Setting Port for Heroku

--- devServer.js
+++ devServer.js
@@ -6,6 +6,8 @@ var config = require('./webpack.config.dev');
 var app = express();
 var compiler = webpack(config);
 
+var port = process.env.PORT || 3000;
+
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath
@@ -19,7 +21,7 @@ app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(3000, '0.0.0.0', function(err) {
+app.listen(port, '0.0.0.0', function(err) {
   if (err) {
     console.log(err);
     return;
